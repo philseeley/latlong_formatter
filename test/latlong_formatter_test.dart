9eae7dabfab35e4f44416604e0273fc4a593dbcc
Phil Seeley <phil.seeley@gmail.com>
2023-08-14 12:09:19 +0300

    Updated tests to deal with the local timezone.

--- test/latlong_formatter_test.dart
+++ test/latlong_formatter_test.dart
@@ -1,19 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:latlong_formatter/latlong_formatter.dart';
+import 'package:format/format.dart' as fmt;
 
 void main() {
-  // NOTE: the expected timezone needs to be updated to the current one.
   test('formatting', () {
     DateTime dt = DateTime.parse('2002-02-27T14:00:00');
-    // String tz = '0300';
+    String tzh = fmt.format('{:+03d}', dt.timeZoneOffset.inHours);
+    String tzm = fmt.format('{:02d}', dt.timeZoneOffset.inMinutes%60);
 
     LatLongFormatter geoFormatter = LatLongFormatter('Leading\nlines\n\n{latd\u00B0m"s\' c},{lond\u00B0m"s\' c}\n{lat+0d.d m.m s.s} {lon+0d.d m.m s.s}\n{user} {pass}\nUTC={utcyyyy-MM-dd HH:mm} Local={localyyyy-MM-dd HH:mm} TZ={tz:}\n\ntrailing\nlines');
 
     expect(geoFormatter.format(Location(9.99999999, 99.99999999), dateTime: dt, username: 'myUser', password: 'myPass'),
-        'Leading\nlines\n\n9\u00B059"59\' N,99\u00B059"59\' E\n+10.0 60.0 60.0 +100.0 60.0 60.0\nmyUser myPass\nUTC=2002-02-27 11:00 Local=2002-02-27 14:00 TZ=+03:00\n\ntrailing\nlines');
+        'Leading\nlines\n\n9\u00B059"59\' N,99\u00B059"59\' E\n+10.0 60.0 60.0 +100.0 60.0 60.0\nmyUser myPass\nUTC=2002-02-27 11:00 Local=2002-02-27 14:00 TZ=$tzh:$tzm\n\ntrailing\nlines');
     expect(geoFormatter.format(Location(-9.99999999, -99.99999999), dateTime: dt, username: 'myUser', password: 'myPass'),
-        'Leading\nlines\n\n9\u00B059"59\' S,99\u00B059"59\' W\n-10.0 60.0 60.0 -100.0 60.0 60.0\nmyUser myPass\nUTC=2002-02-27 11:00 Local=2002-02-27 14:00 TZ=+03:00\n\ntrailing\nlines');
+        'Leading\nlines\n\n9\u00B059"59\' S,99\u00B059"59\' W\n-10.0 60.0 60.0 -100.0 60.0 60.0\nmyUser myPass\nUTC=2002-02-27 11:00 Local=2002-02-27 14:00 TZ=$tzh:$tzm\n\ntrailing\nlines');
 
     geoFormatter = LatLongFormatter('{lat-d m s.sss},{lon-d m s.sss}');
     expect(geoFormatter.format(Location(9.99999999, 99.99999999)), '9 59 60.000,99 59 60.000');
@@ -28,13 +29,13 @@ void main() {
     // Predict Wind format.
     geoFormatter = LatLongFormatter('{user} {lat-d.ddddd} {lon-d.ddddd} {localyyyy-MM-dd HH:mm}{tz}');
     expect(geoFormatter.format(Location(9.99999999, 99.99999999), dateTime: dt, username: 'myUser'),
-        'myUser 10.00000 100.00000 2002-02-27 14:00+0300');
+        'myUser 10.00000 100.00000 2002-02-27 14:00$tzh$tzm');
     expect(geoFormatter.format(Location(-9.99999999, -99.99999999), dateTime: dt, username: 'myUser'),
-        'myUser -10.00000 -100.00000 2002-02-27 14:00+0300');
+        'myUser -10.00000 -100.00000 2002-02-27 14:00$tzh$tzm');
     expect(geoFormatter.format(Location(9.5, 99.5), dateTime: dt, username: 'myUser'),
-        'myUser 9.50000 99.50000 2002-02-27 14:00+0300');
+        'myUser 9.50000 99.50000 2002-02-27 14:00$tzh$tzm');
     expect(geoFormatter.format(Location(-9.5, -99.5), dateTime: dt, username: 'myUser'),
-        'myUser -9.50000 -99.50000 2002-02-27 14:00+0300');
+        'myUser -9.50000 -99.50000 2002-02-27 14:00$tzh$tzm');
 
     // No Foreign Land format.
     geoFormatter = LatLongFormatter('LAT|{latd|m.mmm|c}\nLON|{lond|m.mmm|c}');
